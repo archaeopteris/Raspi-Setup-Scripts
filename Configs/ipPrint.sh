35f694e9b6cc1f39f3586a6e96b86600be5d5b60
Haik Voskanyan <voskanyanhaik@gmail.com>
2017-11-28 21:15:06 +0400

    Update ipPrint.sh

--- Configs/ipPrint.sh
+++ Configs/ipPrint.sh
@@ -1,4 +1,11 @@
+#!/bin/bash
+#This script Prints the IP address of Raspberry Pi's Ethernet port at the end of boot process.
+
 ipParams=$(ifconfig eth0)
+
+YELLOW='\033[0;33m' # Yellow Font Color
+GREEN='\033[1;32m' # Green Font Color
+NC='\033[0m' # No Color
 
 ipv4B=$(echo $ipParams | grep -b -o "inet " | awk 'BEGIN {FS=":"}{print $1}')
 ipv4E=$(echo $ipParams | grep -b -o "netmask " | awk 'BEGIN {FS=":"}{print $1}')
@@ -6,4 +13,8 @@ ipv4E=$(echo $ipParams | grep -b -o "netmask " | awk 'BEGIN {FS=":"}{print $1}')
 ipv4B="$((ipv4B + 14))"
 ipv4Length="$((ipv4E - ipv4B + 10))"
 
+echo -e "${GREEN}======================================${NC}"
+echo -e "${YELLOW}IP Address of Ethernet Port is:${NC}"
 echo ${ipParams:$ipv4B:$ipv4Length}
+echo ""
+echo -e "${GREEN}======================================${NC}"
