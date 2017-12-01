3ad5f1507dc58f1d99f3204d3999d0dbfee6d78a
Haik Voskanyan <voskanyanhaik@gmail.com>
2017-12-01 20:03:50 +0400

    Update ipPrint.sh

--- Configs/ipPrint.sh
+++ Configs/ipPrint.sh
@@ -1,5 +1,9 @@
 #!/bin/bash
-#This script Prints the IP address of Raspberry Pi's Ethernet port at the end of boot process.
+
+# This script gets the ip address of ethernet port Raspberry pi
+# is connected to end sends it to voskanyanhaik@gmail.com by email.
+# Requires sendmail to be installed.
+# Use sudo apt-get install sendmail to install it.
 
 ipParams=$(ifconfig eth0)
 
@@ -13,8 +17,22 @@ ipv4E=$(echo $ipParams | grep -b -o "netmask " | awk 'BEGIN {FS=":"}{print $1}')
 ipv4B="$((ipv4B + 14))"
 ipv4Length="$((ipv4E - ipv4B + 10))"
 
-echo -e "${GREEN}======================================${NC}"
-echo -e "${YELLOW}IP Address of Ethernet Port is:${NC}"
-echo ${ipParams:$ipv4B:$ipv4Length}
+# Output colored code in terminal
+# echo -e "Subject: RPi Address"
+# echo -e "${GREEN}======================================${NC}"
+# echo -e "${YELLOW}IP Address of Ethernet Port is:${NC}"
+# echo ${ipParams:$ipv4B:$ipv4Length}
+# echo ""
+# echo -e "${GREEN}======================================${NC}"
+
+
+# Send email to specified address
+echo -e "Subject: RPi Address" > /etc/init.d/emailContent.txt
+echo -e "======================================" >> /etc/init.d/emailContent.txt
+echo -e "" >> /etc/init.d/emailContent.txt
+echo -e "IP Address of Ethernet Port is:" >> /etc/init.d/emailContent.txt
+echo ${ipParams:$ipv4B:$ipv4Length} >> /etc/init.d/emailContent.txt
 echo ""
-echo -e "${GREEN}======================================${NC}"
+echo -e "======================================" >> /etc/init.d/emailContent.txt
+
+sendmail voskanyanhaik@gmail.com < /etc/init.d/emailContent.txt < /dev/null &
